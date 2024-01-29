5d8f09e0d35f2da25f9ac9f00126b0d67d674c81
Katerina <troonsply@gmail.com>
2024-01-29 14:24:56 +0300

    hspd 10

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.BASE_PATH,
+    reactStrictMode: true,
+    basePath: '/uncolors',
 };
 
 module.exports = nextConfig
