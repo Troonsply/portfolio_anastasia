55ce577b0aa92f02b5db15a73736a7ec37cbfc7e
Katerina <troonsply@gmail.com>
2024-01-29 15:10:28 +0300

    hspd 16

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/uncolors',
+    images: {
+        unoptimized: true,
+    },
+    output: 'export',
 };
 
 export default nextConfig
